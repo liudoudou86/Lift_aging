01888ff2b754f1c148713e1ce2a36958172d67a5
lz860704 <543972930@qq.com>
2021-06-19 18:25:18 +0800

    update code

--- Lift_aging.py
+++ Lift_aging.py
@@ -13,12 +13,15 @@ from loguru import logger
 def up():
  
     ser = serial.Serial("COM4", 9600, timeout=0.5)
+    ser.flushInput() # 清空缓冲区
     ser.write('@21N#\r\n'.encode()) # 上升摄像机
+    time.sleep(3)
     ser.write('@22N#\r\n'.encode()) # 打开电机
+    logger.debug("上升摄像机")
     while True:
-        line = ser.read(1000) # 持续接收信息1000个字节
-        logger.debug(line)
-        if (re.search(b'@51N#', line)): # 从接收的信息中查找
+        line = ser.read(ser.in_waiting).decode("utf-8") # 读取缓冲区的指令
+        # logger.debug(line)
+        if (re.search('@51N#', line)): # 从接收的信息中查找
             ser.write('@22F#\r\n'.encode()) # 关闭电机
             logger.debug("检测到上光耦，关闭电机")
             ser.close()
@@ -29,12 +32,15 @@ def up():
 def down():
  
     ser = serial.Serial("COM4", 9600, timeout=0.5)
+    ser.flushInput() # 清空缓冲区
     ser.write('@21F#\r\n'.encode()) # 下降摄像机
+    time.sleep(3)
     ser.write('@22N#\r\n'.encode()) # 打开电机
+    logger.debug("下降摄像机")
     while True:
-        line = ser.read(1000) # 持续接收信息1000个字节
-        logger.debug(line)
-        if (re.search(b'@52N#',line)): # 从接收的信息中查找
+        line = ser.read(ser.in_waiting).decode("utf-8") # 读取缓冲区的指令
+        # logger.debug(line)
+        if (re.search('@52N#',line)): # 从接收的信息中查找
             ser.write('@22F#\r\n'.encode()) # 关闭电机
             logger.debug("检测到下光耦，关闭电机")
             ser.close()
@@ -57,7 +63,7 @@ cur_time=time.strftime(format_time,time_tup)
  
 if __name__ == '__main__':
 
-    logger.add('log_{}.txt'.format(cur_time), rotation="100 MB") # 将日志输出到txt文本中
+    logger.add('log_{}.txt'.format(cur_time), rotation="10 MB") # 将日志输出到txt文本中
     count = 0
     for i in range (0, 1): # 对函数做循环
         up()
